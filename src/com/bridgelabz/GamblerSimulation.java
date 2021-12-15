0034f01199489833b06c609a7c69cc19a439f905
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-15 09:14:59 -0800

    Added: Initializing Stake and Bet

---
+++ src/com/bridgelabz/GamblerSimulation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class GamblerSimulation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambler Simulation");
+	        int total = 100;
+	        int bet =1;
+	        
+		}
+
+	}
+
